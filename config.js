bd8c879f5b497c205b15597b7fd07d70b5c4b5c1
thaonam12345 <87237274+thaonam12345@users.noreply.github.com>
2021-07-11 07:51:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Yêu em từ lần gặp đầu tiên =)))))',
-    btnNo: 'Không, Panda à  :))',
+    btnNo: 'Không, Chọn không em đấm :)))  :))',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'em biết mà 🥰. yêu anh nhiều nhiều 😘😘',
-    messDesc: 'Tối hôm nào anh học xong sang LB đón em nha',
+    messDesc: 'Tối hôm nào anh học xong sang LB đón em đi đạp vịt nha',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
